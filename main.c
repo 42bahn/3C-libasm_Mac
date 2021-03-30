3abe8239253a9720b567bb610c9b079a96ee21ed
42bahn <bbu0704@gmail.com>
2021-03-30 16:16:36 +0900

    ft_strdup

--- main.c
+++ main.c
@@ -16,13 +16,11 @@ ssize_t _ft_write(int fd, const void *buf, size_t count);
 ssize_t _ft_read(int fd, const void *buf, size_t count);
 char *_ft_strcpy(char *dest, const char *src);
 int _ft_strcmp(const char *s1, const char *s2);
+char *_ft_strdup(const char *s);
 
 int	main(void)
 {
-	printf("%d\n", _ft_strcmp("abcdz", "abcde"));
-	printf("%d\n", _ft_strcmp("abcda", "abcde"));
-	printf("%d\n", _ft_strcmp("abcd", "abcde"));
-	printf("%d\n", _ft_strcmp("abcdeeeee", "abcde"));
+	printf("%s\n", _ft_strdup("abcde"));
 	
 
 /* Linux
