ba15a5c36d080a2b813ee8eb343cc6b8f82f5562
42bahn <bbu0704@gmail.com>
2021-04-08 13:25:25 +0900

    ___error

--- srcs/ft_read.s
+++ srcs/ft_read.s
@@ -1,7 +1,22 @@
 section	.text
 	global	_ft_read
+	extern	___error
 
 _ft_read:
 	mov	rax, 2000003
 	syscall
+	
+	jc	set_errno
+	
+	ret
+
+set_errno:
+	push	rax
+	
+	call	___error
+
+	pop	rbx
+	mov	[rax], rbx
+
+	mov	rax, -1
 	ret
